bdcb5b3bd9fe9d80d8c05bb6b66014f12b32ea69
petr-tik <petr-tik@users.noreply.github.com>
2025-01-05 17:43:55 +0000

    formatting

--- server/app.py
+++ server/app.py
@@ -36,5 +36,7 @@ def get_cards():
     except KeyError:
         result = {'error': True, 'cards': [], 'links': []}
     return result
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
