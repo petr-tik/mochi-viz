ebdc71a01a852e5d5126b102a86aee45ae0ad8d3
petr-tik <petr-tik@users.noreply.github.com>
2025-01-11 21:38:58 +0000

    Trace the execution of 1 run (#1)
    Use opentelemetry to trace through the execution - will enable us to track
    how many cards were retrieved and how many of those did/didn't have
    embeddings generated for
    
    didn't have a particular reason to use zipkin, got it to work easily enough though
    
    ### Where and how to visualise spans
    
    `$ docker compose up`
    Open this page in the browser
    http://localhost:9411/zipkin/
    
    the span will be published there

--- server/app.py
+++ server/app.py
@@ -5,6 +5,25 @@ from cluster import make_links
 from flask import Flask, request
 import requests
 
+from opentelemetry import trace
+from opentelemetry.exporter.zipkin.proto.http import ZipkinExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+resource = Resource(attributes={
+    SERVICE_NAME: "MOCHI_POWERED_BY_LLMs"
+})
+
+zipkin_exporter = ZipkinExporter(endpoint="http://zipkin:9411/api/v2/spans")
+
+
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(zipkin_exporter)
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer("TRACER_TEST")
+
 # Setup logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('mochi')
@@ -16,16 +35,26 @@ def get_card_page(api_key, bookmark=None):
     return requests.get("https://app.mochi.cards/api/cards", params={"bookmark":bookmark}, auth=(api_key, None)).json()
 
 
-@app.route("/api/cards", methods=['POST'])
-def get_cards():
-    try:
-        cards = []
-        api_key = request.form.get('api_key')  
+def get_all_cards(api_key):
+    cards = []
+    with tracer.start_as_current_span("card_getter") as getter_span:
         response = get_card_page(api_key=api_key)
+        # TODO publish a span for how long it took to retrieve cards
         cards += response['docs']
         while len(response['docs']) > 0 and len(cards) < 500:
             response = get_card_page(api_key, response['bookmark'])
             cards += response['docs']
+        getter_span.set_attribute("cards_retrieved", len(cards))
+    return cards
+
+
+@app.route("/api/cards", methods=['POST'])
+def get_cards():
+    try:
+        api_key = request.form.get('api_key')
+        cards = get_all_cards(api_key)
+
+        # TODO publish a span for how long it took to make all the links
         links = make_links(cards)
         result = {'error': False, 'cards': cards, 'links': links}
     except KeyError:
@@ -34,4 +63,7 @@ def get_cards():
 
 
 if __name__ == "__main__":
+    with tracer.start_as_current_span("service_start") as parent:
+        parent.set_attribute("LOOK_AT_ME", 696969)
+        parent.add_event("Starting the server")
     app.run(host='0.0.0.0', port=5000, debug=True)
